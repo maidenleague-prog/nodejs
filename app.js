be0c4260d00b8710b40f15aba1df6c185c293406
maidenleague-prog <maidenleague@gmail.com>
2026-01-02 23:42:26 +0100

    app.js

--- app.js
+++ app.js
@@ -18,4 +18,6 @@ app.use((req, res, next) => {
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
+app.get("/extension-status", (req, res) => {
+  res.json({ enabled: true });
 });
